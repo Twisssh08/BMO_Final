c596defd0bb95f28a7b7c89144157756682db15c
Mateo Acosta <twishm08@gmail.com>
2025-05-29 10:05:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -43,8 +43,7 @@ def pagina_baile():
         return
 
     if st.button("¡Reproducir Baile!"):
-        resultado = publicar_baile()
-        client1.publish("BMO_wowki","{'gesto': 'Baile'}",qos=0, retain=False)
+        resultado = client1.publish("BMO_wowki","{'gesto': 'Baile'}",qos=0, retain=False)
         st.audio(audio_bytes, format="audio/mp3")
         if resultado.rc == 0:
             st.success("✅ Motores activados en Wokwi (mensaje MQTT enviado).")
